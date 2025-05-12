7330127c70489f0b8cd1db72ed5b7d140c82d8dc
RayZ3R0 <iamrayyanahsan@gmail.com>
2025-05-13 00:34:05 +0600

    Add emoji option to custom role icon command
    Support using custom Discord emojis in addition to image upload and URL options for setting custom role icons.

--- Commands/Slash/Utils/customroleicon.js
+++ Commands/Slash/Utils/customroleicon.js
@@ -21,13 +21,19 @@ export default {
       option
         .setName("image")
         .setDescription("Upload an image to use as the role icon")
-        .setRequired(false)
+        .setRequired(false),
     )
     .addStringOption((option) =>
       option
         .setName("url")
         .setDescription("URL of the image to use as the role icon")
-        .setRequired(false)
+        .setRequired(false),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("emoji")
+        .setDescription("Use an emoji as the role icon (can be custom emoji)")
+        .setRequired(false),
     ),
   category: "Utils",
   botPermissions: [PermissionFlagsBits.ManageRoles],
@@ -40,33 +46,36 @@ export default {
         return await interaction.reply({
           embeds: [
             createErrorEmbed(
-              "You need to be a server booster to use this command."
+              "You need to be a server booster to use this command.",
             ),
           ],
           ephemeral: true,
         });
       }
 
-      // Get image from attachment or URL
+      // Get image from attachment, URL, or emoji
       const attachment = interaction.options.getAttachment("image");
       const url = interaction.options.getString("url");
+      const emoji = interaction.options.getString("emoji");
 
-      if (!attachment && !url) {
+      const optionsCount = [attachment, url, emoji].filter(Boolean).length;
+
+      if (optionsCount === 0) {
         return await interaction.reply({
           embeds: [
             createErrorEmbed(
-              "Please provide either an image attachment or a URL to the image."
+              "Please provide one of the following: an image attachment, a URL to an image, or an emoji.",
             ),
           ],
           ephemeral: true,
         });
       }
 
-      if (attachment && url) {
+      if (optionsCount > 1) {
         return await interaction.reply({
           embeds: [
             createErrorEmbed(
-              "Please provide either an image attachment or a URL, not both."
+              "Please provide only one of the following: an image attachment, a URL, or an emoji.",
             ),
           ],
           ephemeral: true,
@@ -86,7 +95,7 @@ export default {
         return await interaction.editReply({
           embeds: [
             createErrorEmbed(
-              "You don't have a custom role yet. Use `/customrole create` first."
+              "You don't have a custom role yet. Use `/customrole create` first.",
             ),
           ],
         });
@@ -98,7 +107,7 @@ export default {
         return await interaction.editReply({
           embeds: [
             createErrorEmbed(
-              "Your custom role was deleted. Please create a new one."
+              "Your custom role was deleted. Please create a new one.",
             ),
           ],
         });
@@ -117,17 +126,41 @@ export default {
           // Download and process attachment
           const downloadedImage = await downloadImage(attachment.url);
           imageBuffer = await processImage(downloadedImage);
-        } else {
+        } else if (url) {
           // Validate and process URL
           validateImageUrl(url);
           const downloadedImage = await downloadImage(url);
           imageBuffer = await processImage(downloadedImage);
+        } else if (emoji) {
+          // Process emoji
+          // Check if it's a custom emoji (format like <:name:id> or <a:name:id>)
+          const emojiRegex = /<(?:a)?:([a-zA-Z0-9_]+):(\d+)>/;
+          const emojiMatch = emoji.match(emojiRegex);
+
+          if (emojiMatch) {
+            // It's a custom emoji
+            const emojiId = emojiMatch[2];
+            const isAnimated = emoji.startsWith("<a:");
+            const emojiUrl = `https://cdn.discordapp.com/emojis/${emojiId}.${isAnimated ? "gif" : "png"}`;
+
+            const downloadedEmoji = await downloadImage(emojiUrl);
+            imageBuffer = await processImage(downloadedEmoji);
+          } else {
+            // It's a default/unicode emoji - not supported for role icons
+            return await interaction.editReply({
+              embeds: [
+                createErrorEmbed(
+                  "Only custom emojis are supported for role icons. Default Discord emojis cannot be used.",
+                ),
+              ],
+            });
+          }
         }
 
         // Set role icon
         await role.setIcon(
           imageBuffer,
-          `Custom role icon set by ${interaction.user.tag}`
+          `Custom role icon set by ${interaction.user.tag}`,
         );
 
         // Create preview attachment
@@ -138,7 +171,7 @@ export default {
         return await interaction.editReply({
           embeds: [
             createSuccessEmbed(
-              "Custom role icon has been updated successfully!"
+              "Custom role icon has been updated successfully!",
             ).setImage("attachment://icon-preview.png"),
           ],
           files: [preview],
@@ -148,7 +181,7 @@ export default {
         return await interaction.editReply({
           embeds: [
             createErrorEmbed(
-              error.message || "An error occurred while processing the image."
+              error.message || "An error occurred while processing the image.",
             ),
           ],
         });
