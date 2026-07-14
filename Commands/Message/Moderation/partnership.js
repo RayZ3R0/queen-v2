973b45ded71a0b06e3cd720cfaf45a7bf71d0597
RayZ3R0 <iamrayyanahsan@gmail.com>
2026-07-14 22:16:40 +0600

    fix(partnership): update partnership advertisement content for clarity and engagement

--- Commands/Message/Moderation/partnership.js
+++ Commands/Message/Moderation/partnership.js
@@ -14,36 +14,33 @@ export default {
   run: async ({ client, message, args, prefix }) => {
     try {
       // Partnership advertisement content.
-      const partnershipContent = `╲⠀╲⠀╲ ╲
-⠀╲⠀╲⠀☆ ⠀ ╲ ⠀⠀⠀⠀⠀
-⠀ ☆⠀ ╲⠀⠀⠀⠀⠀⠀⠀ ★
-⠀⠀⠀ ⠀⠀★
-__**Ｗｅｌｃｏｍｅ　Ｔｏ　＊：･ﾟ✧𝕶𝖚𝖗𝖚𝖒𝖎'𝖘 𝕰𝖒𝖕𝖎𝖗𝖊　駅院ン !**__
-︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵
-｛‧͙⁺˚･༓☾ ｝:･ﾟ✧Kurumi's Empire, is a safe for work and wholesome community! We offer countless of things here to do at this server! The server is also a support server for the Kurumi bot so ask away if you have problems with the bot!
+      const partnershipContent = `
+      # ･ﾟ✧𝕶𝖚𝖗𝖚𝖒𝖎'𝖘 𝕰𝖒𝖕𝖎𝖗𝖊　駅院ン
 
-:ribbon: ◌ Meet new people from different countries as everyone here is allowed to join! You will have a lot of cute and lovely Kurumi and Queen emojis from the server, and so much more! Come and join *:･ﾟ✧Kurumi's Empire for a lovely time! ꒱ ꒱ ୨୧
+## ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵
+｛‧͙⁺˚･༓☾ ｝:･ﾟ✧Kurumi's Empire, a community centered around **Kurumi Tokisaki from the Date A Live series**!
+
+ ◌ Kurumi fans from around the globe converge here! It is here that we appreciate Best Girl, but there is so much more to see and do in our lovely community! ꒱ ꒱ ୨୧
 ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵ ︶ ︵
+
 ｛✧･ﾟ: ✧･ﾟ ｝𝐇𝐞𝐫𝐞 𝐀𝐫𝐞 𝐒𝐨𝐦𝐞 𝐓𝐡𝐢𝐧𝐠𝐬 𝐖𝐞 𝐎𝐟𝐟𝐞𝐫 (๑>◡<๑)
-+ 　　　 　 ·　 * ✫ 　　 * ⊹ * ˚ 　　　 　. . 　　　 　　· 　 ⋆
-╭─────────────── ・*。 . 　˚ 　　　 ⋆
 
-:star:꒱ Bot support for Kurumi bot!
-:crescent_moon: ꒱ Partnerships!
-:star: ꒱ Join if you are also a Date a Live fan!
-:crescent_moon: ꒱ Giveaways coming soon when we have more members!
-:star: ꒱ Interact with our Exclusive bot White Queen!
-:crescent_moon: ꒱ A friendly and easy-going community
-:star: ꒱ Cute and Aesthetically pleasing ~
-:crescent_moon: ꒱ Self Assignable Roles
-:star: ꒱ An exclusive chatbot named Nia-San!
-:crescent_moon: ꒱ Levelling function, with level roles!
-:star: ꒱ And much more coming up!
-︶︶︶︶︶︶︶︶︶︶︶┊
-✦┊ Watch this server grow with us! :heart:
+⭐ Partnerships!
+🌙 Primarily Kurumi Tokisaki-themed, but join us if you are also a DAL fan!
+⭐ Giveaways and streams!
+🌙 Interact with our exclusive bots White Queen and Kurumi!
+⭐ A friendly and easy-going community!
+🌙 Cute and aesthetically pleasing stickers and emojis!
+⭐ Self-assignable roles!
+🌙 Unique DAL-themed level roles!
+⭐ And much more!
 
-Invite link:
-https://discord.gg/kurumin`;
+︶︶︶︶︶︶︶︶︶︶
+
+✦ Come join our community as we grow and prosper under the watch of our glorious waifu Kurumi! 
+
+https://discord.gg/kurumi
+      `;
 
       await message.channel.send({ content: partnershipContent });
     } catch (error) {
